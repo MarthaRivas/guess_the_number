0af12138d1542c7eade85a33b0bdf9faabaca6df
Martha Rivas <Tsubaki@MBP-van-Martha.lan>
2018-09-27 07:33:28 +0200

    creacion de una store para redux vacia

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux'
+// state
+const initialState = {
+    number : undefined,
+    guesses :undefined,
+}
+
+// actions
+
+
+// reducer
+function reducer(state, action) {}
+
+const store = createStore(reducer, initialState)
+
+export default store
